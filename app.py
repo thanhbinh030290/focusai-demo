ce472ca47cc636076c5587e71faa821bfdbcfa7e
thanhbinh030290 <hoathanhbinh.1990@gmail.com>
2026-02-24 14:18:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "FocusAI 2.0 is running successfully!"
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
